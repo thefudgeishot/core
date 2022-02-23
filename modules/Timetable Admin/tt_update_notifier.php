1e59d1a20a7fed676f13b2faa48d1c1054b7bd7c
Vaughan <hoethefudge@gmail.com>
2022-02-23 14:41:20 +0800

    Implemented Close modal view. Implemented Student event notifier.
    Unfinished implementation of timetable update notifier.

--- modules/Timetable Admin/tt_update_notifier.php
+++ modules/Timetable Admin/tt_update_notifier.php
@@ -1,15 +1,23 @@
 <?php
 
+use Gibbon\Domain\System\SettingGateway;
 use Gibbon\Services\Format;
+use Gibbon\Comms\NotificationEvent;
+use Gibbon\Comms\NotificationSender;
+use Gibbon\Forms\CustomFieldHandler;
 use Gibbon\Domain\System\NotificationGateway;
+use Gibbon\Domain\Students\StudentNoteGateway;
+use Gibbon\Domain\IndividualNeeds\INAssistantGateway;
+use Gibbon\Data\Validator;
+
 
 // Send notification once called with | php /modules/Timetable Admin/tt_update_notifier.php
-            $dataDetail = array('gibbonSchoolYearID' => $session->get('gibbonSchoolYearID'), 'gibbonPersonID' => $gibbonPersonID);
-            $sqlDetail = 'SELECT gibbonYearGroupID FROM gibbonFormGroup JOIN gibbonStudentEnrolment ON (gibbonStudentEnrolment.gibbonFormGroupID=gibbonFormGroup.gibbonFormGroupID) JOIN gibbonPerson ON (gibbonStudentEnrolment.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE gibbonStudentEnrolment.gibbonSchoolYearID=:gibbonSchoolYearID AND gibbonStudentEnrolment.gibbonPersonID=:gibbonPersonID';
-            $resultDetail = $connection2->prepare($sqlDetail);
-            $resultDetail->execute($dataDetail);
-        if ($resultDetail->rowCount() == 1) {
-            $rowDetail = $resultDetail->fetch();
+          //  $dataDetail = array('gibbonSchoolYearID' => $session->get('gibbonSchoolYearID'), 'gibbonPersonID' => $gibbonPersonID);
+          //  $sqlDetail = 'SELECT gibbonYearGroupID FROM gibbonFormGroup JOIN gibbonStudentEnrolment ON (gibbonStudentEnrolment.gibbonFormGroupID=gibbonFormGroup.gibbonFormGroupID) JOIN gibbonPerson ON (gibbonStudentEnrolment.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE gibbonStudentEnrolment.gibbonSchoolYearID=:gibbonSchoolYearID AND gibbonStudentEnrolment.gibbonPersonID=:gibbonPersonID';
+          //  $resultDetail = $connection2->prepare($sqlDetail);
+          //  $resultDetail->execute($dataDetail);
+        // if ($resultDetail->rowCount() == 1) {
+          // $rowDetail = $resultDetail->fetch();
 
             // Initialize the notification sender & gateway objects
             $notificationGateway = new NotificationGateway($pdo);
@@ -18,23 +26,18 @@ use Gibbon\Domain\System\NotificationGateway;
             // Raise a new notification event
             $event = new NotificationEvent('Timetable', 'Updated Timetable Subscriber');
 
-            $staffName = Format::name('', $session->get('preferredName'), $session->get('surname'), 'Staff', false, true);
-            $studentName = Format::name('', $row2['preferredName'], $row2['surname'], 'Student', false);
             $actionLink = "/index.php?q=/modules/Timetable/tt_manage_subscription.php";
 
-            $notificationText = sprintf(__('The timetable has been updated, to update your downloaded calendar please 'Format::link($actionLink, 'download')' it here again.').'<br/><br/>';
+            $notificationText = sprintf(('The timetable has been updated, to update your timetable click the action header').'<br/><br/>');
 
             $event->setNotificationText($notificationText);
             $event->setActionLink($actionLink);
-
-            $event->addScope('gibbonPersonIDStudent', $gibbonPersonID);
-            $event->addScope('gibbonYearGroupID', $rowDetail['gibbonYearGroupID']);
 
             // Add event listeners to the notification sender
             $event->pushNotifications($notificationGateway, $notificationSender);
 
             // Send all notifications
             $notificationSender->sendNotifications();
-        }
 
+            echo('Notification Sent! <a href="javascript:history.go(-1)"> <--back</a>')
  ?>
