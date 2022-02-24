6a773485d694987a2391f5ffe5f6835eea25330a
Vaughan <hoethefudge@gmail.com>
2022-02-24 13:30:55 +0800

    Merge remote-tracking branch 'origin/main' into main

--- modules/Timetable Admin/tt_update_notifier.php
+++ modules/Timetable Admin/tt_update_notifier.php
@@ -10,34 +10,28 @@ use Gibbon\Domain\Students\StudentNoteGateway;
 use Gibbon\Domain\IndividualNeeds\INAssistantGateway;
 use Gibbon\Data\Validator;
 
+$URL = $session->get('absoluteURL').'/index.php?q=/modules/Timetable Admin/tt.php';
 
-// Send notification once called with | php /modules/Timetable Admin/tt_update_notifier.php
-          //  $dataDetail = array('gibbonSchoolYearID' => $session->get('gibbonSchoolYearID'), 'gibbonPersonID' => $gibbonPersonID);
-          //  $sqlDetail = 'SELECT gibbonYearGroupID FROM gibbonFormGroup JOIN gibbonStudentEnrolment ON (gibbonStudentEnrolment.gibbonFormGroupID=gibbonFormGroup.gibbonFormGroupID) JOIN gibbonPerson ON (gibbonStudentEnrolment.gibbonPersonID=gibbonPerson.gibbonPersonID) WHERE gibbonStudentEnrolment.gibbonSchoolYearID=:gibbonSchoolYearID AND gibbonStudentEnrolment.gibbonPersonID=:gibbonPersonID';
-          //  $resultDetail = $connection2->prepare($sqlDetail);
-          //  $resultDetail->execute($dataDetail);
-        // if ($resultDetail->rowCount() == 1) {
-          // $rowDetail = $resultDetail->fetch();
+$notificationGateway = new NotificationGateway($pdo);
+$notificationSender = new NotificationSender($notificationGateway, $gibbon->session);
 
-            // Initialize the notification sender & gateway objects
-            $notificationGateway = new NotificationGateway($pdo);
-            $notificationSender = new NotificationSender($notificationGateway, $gibbon->session);
-
-            // Raise a new notification event
-            $event = new NotificationEvent('Timetable', 'Updated Timetable Subscriber');
+// Raise a new notification event
+$event = new NotificationEvent('Timetable', 'Updated Timetable Subscriber');
 
             $actionLink = "/index.php?q=/modules/Timetable/tt_manage_subscription.php";
 
             $notificationText = sprintf(('The timetable has been updated, to update your timetable click the action header').'<br/><br/>');
 
-            $event->setNotificationText($notificationText);
-            $event->setActionLink($actionLink);
+// Add event listeners to the notification sender
+$event->pushNotifications($notificationGateway, $notificationSender);
 
             // Add event listeners to the notification sender
             $event->pushNotifications($notificationGateway, $notificationSender);
 
+$URL .= "&return=success0"; //TODO: IF THE NOTIFICATION ERRORS, WE MIGHT NOT WANT TO THROW A SUCCESS MESSAGE
+header("Location: {$URL}");
+?>
             // Send all notifications
             $notificationSender->sendNotifications();
 
-            echo('Notification Sent! <a href="javascript:history.go(-1)"> <--back</a>')
  ?>
